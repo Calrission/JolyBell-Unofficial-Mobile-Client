cde517df815e6c2810fa0438186b0e422fa5bbc1
Kavrese <kavrese@gmail.com>
2023-02-09 23:24:21 +0300

    create animation

--- app/src/main/java/com/jolybell/jolybellunofficial/screens/MainActivity.kt
+++ app/src/main/java/com/jolybell/jolybellunofficial/screens/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jolybell.jolybellunofficial.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.appbar.AppBarLayout.LayoutParams
+import com.jolybell.jolybellunofficial.R
 import com.jolybell.jolybellunofficial.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        val params = binding.logoTop.motion.layoutParams
+        params.height = LayoutParams.WRAP_CONTENT
+        binding.logoTop.motion.layoutParams = params
     }
 }
