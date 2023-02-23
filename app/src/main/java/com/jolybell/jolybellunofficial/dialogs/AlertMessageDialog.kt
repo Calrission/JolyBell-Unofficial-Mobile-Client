871c6abd2d01f99a65be25ebb663a9d612e459e2
Kavrese <kavrese@gmail.com>
2023-02-23 21:57:08 +0300

    add full logic for AlertMessageDialog.kt

--- app/src/main/java/com/jolybell/jolybellunofficial/dialogs/AlertMessageDialog.kt
+++ app/src/main/java/com/jolybell/jolybellunofficial/dialogs/AlertMessageDialog.kt
@@ -2,16 +2,35 @@ package com.jolybell.jolybellunofficial.dialogs
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import com.jolybell.jolybellunofficial.R
+import com.jolybell.jolybellunofficial.databinding.DialogAlertMessageBinding
 
 class AlertMessageDialog(
     context: Context,
     themeRes: Int = R.style.CustomDialogTheme,
-    dismiss: Boolean = true
+    dismiss: Boolean = true,
+    val title: String,
+    val message: String,
+    val showCancel: Boolean,
+    val onClickOk: OnClickListener?,
+    val onClickCancel: OnClickListener?
 ): BaseDialog(context, themeRes, dismiss) {
+
+    private lateinit var binding: DialogAlertMessageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = DialogAlertMessageBinding.inflate(layoutInflater)
+
+        binding.titleMessageDialog.text = title
+        binding.messageDialog.text = message
+
+        binding.btnOK.setOnClickListener(onClickOk)
+        binding.btnCancel.setOnClickListener(onClickCancel)
+
+        binding.btnCancel.visibility = if (showCancel) View.GONE else View.VISIBLE
     }
 }
